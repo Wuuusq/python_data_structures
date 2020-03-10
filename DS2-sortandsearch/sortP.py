37214d108ca44cb5e04c56c0c08531aca3f0ad08
Wuuusq <qi729496613@qq.com>
2020-03-10 18:47:50 +0800

    插入排序and希尔排序

--- DS2-sortandsearch/sortP.py
+++ DS2-sortandsearch/sortP.py
@@ -10,6 +10,41 @@ def bubbleSort(arr):
 print(bubbleSort([24, 48, 65, 48, 22, 12, 36, 98, 45]))
 
 
+def bubbleSort(alist):
+    for passnum in range(len(alist) - 1, 0, -1):
+        for i in range(passnum):
+            if alist[i] < alist[i + 1]:
+                temp = alist[i]
+                alist[i] = alist[i + 1]
+                alist[i + 1] = temp
+
+
+alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+bubbleSort(alist)
+print(alist)
+
+
+# 短冒泡排序
+def bubbleSort(alist):
+    flag = True
+    passnum = len(alist) - 1
+
+    while passnum > 0 and flag:
+        flag = False
+        for i in range(passnum):
+            if alist[i] > alist[i + 1]:
+                flag = True
+                temp = alist[i]
+                alist[i] = alist[i + 1]
+                alist[i + 1] = temp
+
+        passnum = passnum - 1
+
+
+alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+bubbleSort(alist)
+print(alist)
+
 
 # 选择排序
 def selectionSort(arr):
@@ -26,3 +61,49 @@ def selectionSort(arr):
 
 
 print(selectionSort([24, 48, 65, 48, 22, 12, 36, 98, 45]))
+
+
+# 插入排序
+def insertionSort(alist):
+    for i in range(1,len(alist)):
+        currentValue = alist[i]
+        pos = i
+        while pos > 0 and alist[pos -1]>currentValue:
+            alist[pos] = alist[pos -1]
+            pos = pos -1
+
+        alist[pos] = currentValue
+
+alist = [54,26,93,17,77,31,44,55,20]
+insertionSort(alist)
+print(alist)
+
+
+# 希尔排序
+def shellSort(alist):
+    sublistcount = len(alist) // 2
+
+    while sublistcount > 0:
+        for startposition in range(sublistcount):
+            gapInsertionSort(alist, startposition, sublistcount)
+
+        print('alist：', alist)
+
+        sublistcount = sublistcount // 2
+
+
+def gapInsertionSort(alist, start, gap):
+    for i in range(start + gap, len(alist), gap):
+        currentValue = alist[i]
+
+        pos = i
+        while pos >= gap and alist[pos - gap] > currentValue:
+            alist[pos] = alist[pos - gap]
+            pos = pos - gap
+
+        alist[pos] = currentValue
+
+
+alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+shellSort(alist)
+print(alist)
