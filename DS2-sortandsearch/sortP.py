899c1dc22bb3f40cad6bddcaaf07727a0247cda2
Wuuusq <qi729496613@qq.com>
2020-03-10 12:16:15 +0800

    冒泡排序and选择排序

---
+++ DS2-sortandsearch/sortP.py
@@ -0,0 +1,28 @@
+# 冒泡排序
+def bubbleSort(arr):
+    for i in range(1, len(arr)):
+        for j in range(0, len(arr) - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+
+print(bubbleSort([24, 48, 65, 48, 22, 12, 36, 98, 45]))
+
+
+
+# 选择排序
+def selectionSort(arr):
+    for i in range(len(arr) - 1):
+        # 记录最小数的索引
+        minIndex = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[minIndex]:
+                minIndex = j
+        # i 不是最小数时，将 i 和最小数进行交换
+        if i != minIndex:
+            arr[i], arr[minIndex] = arr[minIndex], arr[i]
+    return arr
+
+
+print(selectionSort([24, 48, 65, 48, 22, 12, 36, 98, 45]))
